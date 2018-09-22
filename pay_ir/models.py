44d9b3747fb0c7107d754d0b5a9f639d7d19c395
Rasool Safari <safari@rasooll.com>
2018-09-22 21:49:24 +0330

    Edit model fields.

--- pay_ir/models.py
+++ pay_ir/models.py
@@ -1,26 +1,35 @@
 from django.db import models
 
 
-class Invoice(models.Model):
-    """
-    This model use for invoices.
-    """
-
-    full_name = models.CharField(name="Full name", blank=True, null=True)
-    amount = models.BigIntegerField(name="Amount")
-    mobile = models.IntegerField(name="Mobile number")
-    description = models.CharField(name="Description", max_length=255)
-    transid = models.IntegerField(name="Trans ID")
-
-
 class Payment(models.Model):
     """
     This model use for registeration payments.
     """
 
-    invoice = models.ForeignKey(
-        Invoice, on_delete=models.CASCADE, related_name="Invoice")
-    status = models.BooleanField(name="Status", default=False)
-    card_number = models.BigIntegerField(name="Card number")
-    trace_number = models.BigIntegerField(name="Trace number")
-    message = models.TextField(name="Message")
+    full_name = models.CharField(
+        name="Full name", blank=True, null=True
+    )
+    amount = models.BigIntegerField(
+        name="Amount"
+    )
+    mobile = models.IntegerField(
+        name="Mobile number"
+    )
+    description = models.CharField(
+        name="Description", max_length=255, blank=True, null=True
+    )
+    transid = models.IntegerField(
+        name="Trans ID"
+    )
+    status = models.BooleanField(
+        name="Status", default=False
+    )
+    card_number = models.BigIntegerField(
+        name="Card number", blank=True, null=True
+    )
+    trace_number = models.BigIntegerField(
+        name="Trace number", blank=True, null=True
+    )
+    message = models.TextField(
+        name="Message", blank=True, null=True
+    )
