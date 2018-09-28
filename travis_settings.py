8ad130e0373fff233bee47df84165443f75cf3ad
Rasool Safari <safari@rasooll.com>
2018-09-28 13:18:35 +0330

    Fix bug

--- travis_settings.py
+++ travis_settings.py
@@ -1,7 +1,7 @@
 from .settings import *
 
 INSTALLED_APPS += ["pay_ir"]
-ROOT_URLCONF = 'django_payir.travis_urls'
+ROOT_URLCONF = 'django_pj.travis_urls'
 PAY_IR_CONFIG = {
     "api_key": "test"
 }
